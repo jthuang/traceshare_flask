8b6318291d40f6acf04e12d199d882516b3a9aa1
jthuang <jthuang@ischool.berkeley.edu>
2014-04-25 00:00:18 -0700

    Try to add some logging functionalities.

--- traceshare.py
+++ traceshare.py
@@ -2,7 +2,13 @@ import flask
 from os import environ
 
 app = flask.Flask(__name__)
-app.debug = True
+
+# log to stderr
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
 
 @app.route('/')
 def hello():
