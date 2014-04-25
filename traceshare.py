dc00d7d74433040912eeeded6752fe2cf875ddee
jthuang <jthuang@ischool.berkeley.edu>
2014-04-24 23:40:37 -0700

    Added myPlaces.html

--- traceshare.py
+++ traceshare.py
@@ -2,7 +2,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello():
     return 'Hello World! TraceShare!'
+
+@app.route('/myplace', methods=["GET"])
+def get_my_place:
+    app.logger.debug("Redirecting to " + destination)
+    resp = flask.make_response(flask.render_template('myPlaces.html')
+
+    return resp
