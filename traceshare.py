fbe2e415498489cf0135833e6e653abdc9ce668a
jthuang <jthuang@ischool.berkeley.edu>
2014-04-29 16:51:11 -0700

    added basic placeUpdate

--- traceshare.py
+++ traceshare.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 from os import environ
 
 app = flask.Flask(__name__)
@@ -21,9 +22,22 @@ def get_my_place():
     return resp
 
 @app.route('/placeEdit', methods=["GET"])
-def get_my_place():
+def edit_my_place():
     app.logger.debug("Accessing placeEdit.html")
     resp = flask.make_response(flask.render_template('placeEdit.html'))
     return resp
+
+@app.route('/placeUpdate', methods=["POST"])
+def update_my_place():
+    app.logger.debug("Accessing placeUpdate")
+    place_id = request.form.get('placeId', "")
+    date_time = request.form.get('dateTime', "")
+    share_option = request.form.get('shareOption', "")
+    string = place_id + " " + date_time + " " + share_option
+    return place_id
+
+#    resp = flask.make_response(flask.render_template('placeEdit.html'))
+#    return resp
+
 
 
